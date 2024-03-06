db55a44450acd09fc37997c48b4e36050e86fc3b
Adel Ansari <adelansari.a@gmail.com>
2024-03-06 07:32:38 +0200

    handle book deletion and redirect to admin page

--- deletebook.php
+++ deletebook.php
@@ -1,15 +1,30 @@
 <?php
-    // If the user is not logged in, redirect them back to login.php.
+session_start();
+// If the user is not logged in, redirect them back to login.php.
+if (!isset($_SESSION['login'])) {
+    header("Location: login.php");
+    exit();
+}
 
-    // Check the POST parameter "bookid". If it's set, delete the corresponding book from the data file.
-    // Hint: array_diff will not work here, since you'd need to create the whole book "object". Find the index and use array_splice instead.
+// Read the file into array variable $books:
+$books = json_decode(file_get_contents('books.json'), true);
 
-    // Redirect back to admin.php.
+// Check the POST parameter "bookid". If it's set, delete the corresponding book from the data file.
+// Hint: array_diff will not work here, since you'd need to create the whole book "object". Find the index and use array_splice instead.
+
+if (isset($_POST['bookid'])) {
+    $bookid = $_POST['bookid'];
+
+    foreach ($books as $index => $book) {
+        if ($book['id'] == $bookid) {
+            array_splice($books, $index, 1);
+            break;
+        }
+    }
+    file_put_contents('books.json', json_encode($books));
+}
 
 
-    // Read the file into array variable $books:
-    $json = file_get_contents("books.json");
-    $books = json_decode($json, true);
-
-    // Once you have removed the book from the variable $books write it into the file.
-    file_put_contents("books.json", json_encode($books));
+// Redirect back to admin.php.
+header('Location: admin.php');
+exit;
