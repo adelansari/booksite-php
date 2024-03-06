237d6d8af2eec229e5578752cadd82ff4dd6e334
Adel Ansari <adelansari.a@gmail.com>
2024-03-06 06:58:27 +0200

    changing the url query parameter for genre filtering

--- booksite.php
+++ booksite.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +8,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="booksite.css">
 </head>
+
 <body>
     <div id="container">
         <header>
@@ -15,32 +17,32 @@
         <nav id="main-navi">
             <ul>
                 <li><a href="booksite.php">Home</a></li>
-                <li><a href="booksite.php?genre=adventure">Adventure</a></li>
-                <li><a href="booksite.php?genre=classic">Classic Literature</a></li>
-                <li><a href="booksite.php?genre=coming-of-age">Coming-of-age</a></li>
-                <li><a href="booksite.php?genre=fantasy">Fantasy</a></li>
-                <li><a href="booksite.php?genre=historical">Historical Fiction</a></li>
-                <li><a href="booksite.php?genre=horror">Horror</a></li>
-                <li><a href="booksite.php?genre=mystery">Mystery</a></li>
-                <li><a href="booksite.php?genre=romance">Romance</a></li>
-                <li><a href="booksite.php?genre=scifi">Science Fiction</a></li>
+                <li><a href="booksite.php?genre=Adventure">Adventure</a></li>
+                <li><a href="booksite.php?genre=Classic Literature">Classic Literature</a></li>
+                <li><a href="booksite.php?genre=Coming-of-age">Coming-of-age</a></li>
+                <li><a href="booksite.php?genre=Fantasy">Fantasy</a></li>
+                <li><a href="booksite.php?genre=Historical Fiction">Historical Fiction</a></li>
+                <li><a href="booksite.php?genre=Horror">Horror</a></li>
+                <li><a href="booksite.php?genre=Mystery">Mystery</a></li>
+                <li><a href="booksite.php?genre=Romance">Romance</a></li>
+                <li><a href="booksite.php?genre=Science Fiction">Science Fiction</a></li>
             </ul>
         </nav>
         <main>
             <?php
-                // Here you should display the books of the given genre (GET parameter "genre"). Check the links above for parameter values.
-                // If the parameter is not set, display all books.
+            // Here you should display the books of the given genre (GET parameter "genre"). Check the links above for parameter values.
+            // If the parameter is not set, display all books.
 
-                // Use the HTML template below and a loop (+ conditional if the genre was given) to go through the books in file  
-                
-                // You also need to check the cookies to figure out if the book is favorite or not and display correct symbol.
-                // If the book is in the favorite list, add the class "fa-star" to the a tag with "bookmark" class.
-                // If not, add the class "fa-star-o". These are Font Awesome classes that add a filled star and a star outline respectively.
-                // Also, make sure to set the id parameter for each book, so the setfavorite.php page gets the information which book to favorite/unfavorite.
+            // Use the HTML template below and a loop (+ conditional if the genre was given) to go through the books in file  
 
-                // Read the file into array variable $books:
-                $json = file_get_contents("books.json");
-                $books = json_decode($json, true);
+            // You also need to check the cookies to figure out if the book is favorite or not and display correct symbol.
+            // If the book is in the favorite list, add the class "fa-star" to the a tag with "bookmark" class.
+            // If not, add the class "fa-star-o". These are Font Awesome classes that add a filled star and a star outline respectively.
+            // Also, make sure to set the id parameter for each book, so the setfavorite.php page gets the information which book to favorite/unfavorite.
+
+            // Read the file into array variable $books:
+            $json = file_get_contents("books.json");
+            $books = json_decode($json, true);
 
             ?>
             <h2>Genre Name or "All Books"</h2>
@@ -58,6 +60,7 @@
             </section>
 
         </main>
-    </div>    
+    </div>
 </body>
+
 </html>
